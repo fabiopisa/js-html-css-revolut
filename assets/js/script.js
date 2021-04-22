f77018c890b25ffbd67fac8fb9c095c8311c40e4
fabiopisa <pisanellifabio@gmail.com>
2021-04-22 09:16:30 +0200

    primo commit es

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+ /*  var link = $('.nav-button li');
+  var functionality = $('.list-functionality').hide();
+  var subscriptions = $('.list-subscriptions').hide();
+  var who = $('.list-who').hide();
+  var help = $('.list-help').hide(); */
+
+  /* functionality.hide();
+  subscriptions.hide();
+  who.hide();
+  help.hide(); */
+
+  /* link.on('hover', function(){
+    if(link.eq(0)){
+      functionality.show();
+    }
+  }) */
+  /* if(link.eq(0).hover()){
+    functionality.show();
+  } */
+
+
+});
